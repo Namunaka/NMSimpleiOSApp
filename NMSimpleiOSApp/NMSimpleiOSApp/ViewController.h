5bab7b83908b35572a88a558a5ff0d83fabfcdb5
dahuang <huangzhaoqing@gameapp.net>
2017-06-29 15:47:35 +0800

    项目创建

---
+++ NMSimpleiOSApp/NMSimpleiOSApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NMSimpleiOSApp
+//
+//  Created by idolplay-macpro on 2017/6/29.
+//  Copyright © 2017年 namunaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NMSimpleiOSApp/NMSimpleiOSApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NMSimpleiOSApp
+//
+//  Created by idolplay-macpro on 2017/6/29.
+//  Copyright © 2017年 namunaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
